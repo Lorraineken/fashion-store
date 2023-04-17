aa836faddaab483c71c85c560e9c34a5701141a8
Lorraineken <lorrainekupa@gmail.com>
2023-04-17 15:42:41 +0300

    Added create action for users

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+     
+   def create 
+    user = User.create!(user_params)
+    if user.valid? 
+        render json: user, status: :created
+    else 
+        render json:{error:"Invalid user detail"}, status: :unprocessable_entity
+
+    end
+   end
+
 end
