d31ab5af371a06d374b8fde0e0aa1e21fa0afa11
Lorraineken <lorrainekupa@gmail.com>
2023-04-18 15:09:37 +0300

    Added products validations

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ApplicationRecord
-    has_many :comments
+    has_many :reviews
+    has_many :products_categories
+    has_many :categories, through: :products_categories
+
+    validates :name, {
+        presence: true,
+        length: {minimum:4}
+    }
+    validates :price, {
+        presence: true,
+        numericality: {
+            greater_than_or_equal_to: 2
+        }
+    }
+    validates :description, {
+        length: {maximum:30}
+    }
 end
