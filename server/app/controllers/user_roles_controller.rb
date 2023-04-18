a61574df1b3c72ce247a61c85ef3b5119ff82b45
Lorraineken <lorrainekupa@gmail.com>
2023-04-18 11:51:11 +0300

    Added userrole actions

--- server/app/controllers/user_roles_controller.rb
+++ server/app/controllers/user_roles_controller.rb
@@ -1,2 +1,39 @@
 class UserRolesController < ApplicationController
+    def create 
+        user_role = UserRole.create!(reviews_params)
+        if user_role.valid? 
+            render json: user_role, status: :created
+        else 
+            render json:{error:"Invalid user_role detail"}, status: :unprocessable_entity
+    
+        end
+       end
+    
+       def index 
+        user_role =UserRole.all 
+        render json: user_role, status: :ok
+       end
+    
+       def show 
+        user_role = UserRole.find(params[:id])
+        render json: user_role, status: :ok
+       end
+    
+       def update 
+        user_role =UserRole.find(params[:id])
+        user_role.update!(user_role_params)
+        render json: user_role, status: :accepted
+       end
+
+       def destroy 
+        user_role=UserRole.find(params[:id])
+        user_role.destroy 
+        head :no_content
+       end
+    
+       private
+    
+       def user_role_params 
+        params.permit(:user_id,:role_id)
+       end
 end
