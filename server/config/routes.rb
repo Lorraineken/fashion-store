126c4622a8384998b5043d400693bbc8cdcef9eb
Skanner33 <mathewmageto@gmail.com>
2023-04-18 15:39:32 +0300

    removed fallback controller

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
   #authentication routes
   post "/auth/login", to: "authentication#login_account"
   post "/auth/register", to: "authentication#create_account"
