97e81677442901810548ec27f9b90f4fe9dc3b0d
Skanner33 <mathewmageto@gmail.com>
2023-04-18 09:25:48 +0300

    created authentication controller

--- server/app/models/order.rb
+++ server/app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  enum :status, [:pending, :completed, :cancelled]
+  belongs_to :users
+
+  has_many :payments
+
+  validates :quality, {
+              presence: true,
+            }
 end
