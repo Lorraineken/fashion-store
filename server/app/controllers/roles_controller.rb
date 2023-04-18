08b00ec7172cf4e0d90f46b29c53c71033b6c661
Lorraineken <lorrainekupa@gmail.com>
2023-04-18 13:05:16 +0300

    Added roles actions

--- server/app/controllers/roles_controller.rb
+++ server/app/controllers/roles_controller.rb
@@ -1,2 +1,39 @@
 class RolesController < ApplicationController
+    def create 
+        role = Role.create!(role_params)
+        if role.valid? 
+            render json: role, status: :created
+        else 
+            render json:{error:"Invalid role detail"}, status: :unprocessable_entity
+    
+        end
+       end
+    
+       def index 
+        role =Role.all 
+        render json: role, status: :ok
+       end
+    
+       def show 
+        role = Role.find(params[:id])
+        render json: role, status: :ok
+       end
+    
+       def update 
+        role =Role.find(params[:id])
+        role.update!(role_params)
+        render json: role, status: :accepted
+       end
+
+       def destroy 
+        role=Role.find(params[:id])
+        role.destroy 
+        head :no_content
+       end
+    
+       private
+    
+       def role_params 
+        params.permit(:name)
+       end
 end
