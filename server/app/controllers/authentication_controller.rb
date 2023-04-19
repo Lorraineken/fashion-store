8ccc73876ff81fb87a4a539d7ecc6c8278f1ea22
Lorraineken <lorrainekupa@gmail.com>
2023-04-19 10:26:56 +0300

    Updated authentication

--- server/app/controllers/authentication_controller.rb
+++ server/app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
   def create_account
     user = User.create(create_params)
     if user.valid?
-      create_user_session(user.id, user.user_type)
+      create_user_session(user.id)
       app_response(status_code: 201, message: "Account created successfully", body: user)
     else
       app_response(status_code: 422, message: "Invalid input", body: user.errors.full_messages)
@@ -12,7 +12,7 @@ class AuthenticationController < ApplicationController
   def login_account
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
-      create_user_session(user.id, user.user_type)
+      create_user_session(user.id)
 
       app_response(message: "Log in success", body: user)
     else
@@ -33,16 +33,14 @@ class AuthenticationController < ApplicationController
   private
 
   def create_params
-    params.permit(:name, :email, :password, :display_picture, :user_type)
+    params.permit(:name, :email, :password, :display_picture)
   end
 
-  def create_user_session(user_id, user_type)
+  def create_user_session(user_id)
     session[:user_id] ||= user_id
-    session[:user_type] ||= user_type
   end
 
   def delete_user_session
     session.delete :user_id
-    session.delete :user_type
   end
 end
