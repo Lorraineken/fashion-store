fc0873051bd7d3d64867a16581e7d061d7b3b1d6
Lorraineken <lorrainekupa@gmail.com>
2023-04-20 09:52:26 +0300

    Updated payments table

--- server/db/migrate/20230417121305_create_payments.rb
+++ server/db/migrate/20230417121305_create_payments.rb
@@ -1,9 +1,9 @@
 class CreatePayments < ActiveRecord::Migration[6.1]
   def change
     create_table :payments do |t|
-      t.integer :order_id
-      t.string :payment_method
-      t.string :amount
+      t.belongs_to :order, null: false, foreign_key: true
+      t.string :payment_method, null: false
+      t.string :amount, null:false
       t.string :status
 
       t.timestamps
