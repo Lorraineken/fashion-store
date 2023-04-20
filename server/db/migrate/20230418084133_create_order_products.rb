43fa858bcb3e9ed0f68d5196f5ec1dcfd89a25f8
Lorraineken <lorrainekupa@gmail.com>
2023-04-20 09:56:58 +0300

    Updated order_products table

--- server/db/migrate/20230418084133_create_order_products.rb
+++ server/db/migrate/20230418084133_create_order_products.rb
@@ -1,8 +1,8 @@
 class CreateOrderProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :order_products do |t|
-      t.integer :order_id
-      t.integer :product_id
+      t.belongs_to :order, null: false, foreign_key: true
+      t.belongs_to :product, null: false, foreign_key: true
       t.float :quantity
 
       t.timestamps
