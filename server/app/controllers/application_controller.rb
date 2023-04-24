640433345e91dba2189798fb8416686a20b3472e
barsu5489 <ebarsulai@gmail.com>
2023-04-24 21:03:31 +0300

    stash

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  JWT_SECRET = "qqqwrcgff21234OCKS)(WJMW)"
+
+  wrap_parameters format: []
+
   # application response body
   def app_response(status_code: 200, message: "Success", body: nil, serializer: nil)
     if serializer
@@ -21,6 +25,34 @@ class ApplicationController < ActionController::API
   def authorize
     unauthorized unless session.include? :user_id
   end
+
+  # authorize using JWT
+  def authorize_jwt
+    jwt_data = decode_data(request.headers["token"])
+    app_response(body: jwt_data)
+  end
+
+  def uid
+    jwt_data = decode_data(request.headers["token"])
+    jwt_data[0]["user_id"]
+  end
+
+  # ENCODE DATA INTO TOKEN
+  def encode_data(data)
+    # set expiry of token (I have currently set it at 24 hours)
+    # data[:exp] = 24.hours.from_now.to_i
+    JWT.encode(data, JWT_SECRET, "HS256")
+  end
+
+  # DECODE INFORMATION FROM TOKEN
+  def decode_data(token)
+    begin
+      JWT.decode(token, key: JWT_SECRET, verify: true, options: { algorithm: "HS256" })
+    rescue => error
+      error
+    end
+  end
+
   def authorize_admin
     return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :admin_user
   end
--- server/app/controllers/authentication_controller.rb
+++ server/app/controllers/authentication_controller.rb
@@ -3,7 +3,8 @@ class AuthenticationController < ApplicationController
     user = User.create(create_params)
     if user.valid?
       create_user_session(user.id)
-      app_response(status_code: 201, message: "Account created successfully", body: user)
+      token = encode_token(user_id: user.id)
+      app_response(status_code: 201, message: "Account created successfully", body: { user: user, token: token })
     else
       app_response(status_code: 422, message: "Invalid input", body: user.errors.full_messages)
     end
@@ -13,13 +14,22 @@ class AuthenticationController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       create_user_session(user.id)
-      
+      data = {
+        user_id: user.id,
+        user_type: user.user_type,
+      }
+      token = encode_data(data)
+      app_response(status_code: 200, message: "Login successful", body: {
+                     user: ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer),
+                     token: token,
+                   })
+
       #check for admin
-      role = user.roles.where(name: "admin").first
-      if role.name == "admin"
-        session[:admin_user] = "admin"
-      end
-      
+      # role = user.roles.where(name: "admin").first
+      # if role.name == "admin"
+      #   session[:admin_user] = "admin"
+      # end
+
       app_response(message: "Log in success", body: user)
     else
       app_response(status_code: 401, message: "Invalid username or password")
@@ -31,7 +41,7 @@ class AuthenticationController < ApplicationController
     delete_admin_session
     app_response(status_code: 200, message: "Log out successfully")
   end
-  
+
   # Render 404 page for unmapped routes
   def render_404
     not_found
@@ -51,7 +61,8 @@ class AuthenticationController < ApplicationController
     session.delete :user_id
   end
 
-  def delete_admin_session 
+  def delete_admin_session
     session.delete :admin_user
   end
+
 end
